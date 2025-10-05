1b083ff70ff454da8c0717179e3601e270a22435
Irfan Ali <irfanthavi@gmail.com>
2025-10-05 16:22:38 +0500

    fix image and hadding

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import HeroSection from './components/sections/hero-section'
 import Footer from './components/sections/footer'
 import Contact from './components/sections/contact'
+import AwardRecognition from './components/sections/award-recognition'
 
 function App() {
   return (
     <div>
-      {/* <HeroSection /> */}
+      <HeroSection />
+      <AwardRecognition/>
       <Contact/>
       <Footer/>
     </div>
---
+++ src/components/sections/award-recognition.jsx
@@ -0,0 +1,14 @@
+export default function AwardRecognition() {
+    return (
+        <section className="relative w-full bg-cover bg-center bg-no-repeat h-[675px] flex flex-col justify-start items-center">
+            <h1 className="font-[Segoe UI] font-bold text-[66px] leading-[64px] tracking-normal text-black mt-20">
+                Awards and Recognitions
+            </h1>
+            <img
+                src="/images/award-recognition.png"
+                alt="Awards and Recognition"
+                className="h-[572px] object-contain  -mt-8"    
+            />
+        </section>
+    );
+}
