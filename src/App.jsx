cbfa0d20ea6862f81cd7eceb0e17331f2e2c8b3d
Irfan Ali <irfanthavi@gmail.com>
2025-10-06 16:35:42 +0500

    fix: image para heading

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import HeroSection from './components/sections/hero-section'
 import Footer from './components/sections/footer'
 import Contact from './components/sections/contact'
+import Technology from './components/sections/technology'
 
 function App() {
   return (
     <div>
       {/* <HeroSection /> */}
       <Contact/>
+      <Technology/>
       <Footer/>
     </div>
   )
