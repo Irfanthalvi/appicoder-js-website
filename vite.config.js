cec7cbc247d09f3598920c745d5425548a4a08a7
Irfan Ali <irfanthavi@gmail.com>
2025-10-10 16:28:11 +0500

    prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // 👈 important for main-branch hosting
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
