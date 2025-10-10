9ca0326a678555fb5cfb6095c40811ee43fd600a
Irfan Ali <irfanthavi@gmail.com>
2025-10-10 10:14:41 +0500

    Resolved merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import HeroSection from './components/sections/hero-section'
 import Footer from './components/sections/footer'
 import Contact from './components/sections/contact'
-<<<<<<< HEAD
 import AboutAndServices from './components/sections/about-and-services'
 import WhyChooseUs from './components/sections/why-choose-us'
 import AwardRecognition from './components/sections/award-recognition'
 import Products from './components/sections/products'
 import Technology from './components/sections/technology'
 import Industries from './components/sections/industries'
-=======
 import TestimonialSection from './components/sections/testimonial-section'
->>>>>>> testimonial-section
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <HeroSection />
       <AboutAndServices />
       <WhyChooseUs />
@@ -24,13 +20,8 @@ function App() {
       <Technology/>
       <Industries/>
       <Contact />
-      <Footer />
-=======
-      {/* <HeroSection /> */}
-      {/* <Contact/> */}
       <TestimonialSection/>
-      <Footer/>
->>>>>>> testimonial-section
+      <Footer />
     </div>
   )
 }
